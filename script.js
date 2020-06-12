ec0d163cef309fb97ad509607c98723ba9499f45
kachi <52211783+fudosman@users.noreply.github.com>
2020-06-12 12:08:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "75%";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
